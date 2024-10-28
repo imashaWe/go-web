c3bf10bb68cdbe40489aaf5f52d73bd64eba8e0e
Imasha Weerakoon <imasha98.we@gmail.com>
2024-10-28 18:53:13 +0530

    Refactor route path in main.go

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/gin-gonic/gin";
+
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/hello-world", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World!",
 		})
